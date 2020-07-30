78722c630b567f10d7a273a9299eb2c45db2cb0a
Purvi Harniya <42908278+Purviharniya@users.noreply.github.com>
2020-07-30 14:36:36 +0530

    Update AboutusCarousel.js

--- src/Pages/About-us Mobile/AboutusCarousel.js
+++ src/Pages/About-us Mobile/AboutusCarousel.js
@@ -1,91 +1,81 @@
 import React, { useState } from 'react';
-import {Carousel,CarouselItem,CarouselControl,CarouselIndicators} from 'reactstrap';
 import './AboutusCarousel.css';
 
-const items = [
-  {
-    src: require('./images/team/1.png'),
-    name: 'Anustup Mukherjee',
-    desig: 'CEO'
-  },
-  {
-    src: require('./images/team/2.png'),
-    name: 'Ayush Tripathi',
-    desig: 'COO & CO-FOUNDER'
-  },
-  {
-    src: require('./images/team/3.png'),
-    name: 'Ashish Sharma',
-    desig: 'COO & CO-FOUNDER'
-  },
-  {
-    src: require('./images/team/4.png'),
-    name: 'Ankit Raj',
-    desig: 'CTO'
-  },
-  {
-    src: require('./images/team/5.png'),
-    name: 'Ipshita Prosad',
-    desig: 'HOP'
-  },
-  {
-    src: require('./images/team/6.png'),
-    name: 'Aditya Kumar',
-    desig: 'DIRECTOR'
-  },
-
-];
 
 const AboutusCarousel = (props) => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
-
-  const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  }
-
-  const previous = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  }
-
-  const goToIndex = (newIndex) => {
-    if (animating) return;
-    setActiveIndex(newIndex);
-  }
-
-  const slides = items.map((item) => {
-    return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={item.src}
-      >
-          <div className="text-center">
-            <img src={item.src} alt={item.name} width="60%" height="50%" className="team-imgs"/>
-        <h5 className="head-name">{item.name}</h5>
-        <h6  className="pb-5 head-design"> {item.desig}</h6>
-        </div>
-       
-      </CarouselItem>
-    );
-  });
-
   return (
-    <Carousel
-      activeIndex={activeIndex}
-      next={next}
-      previous={previous}
-      interval={3000}
-    >
-      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} className="indicate"/>
-      {slides}
-      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} className="control-car1"/>
-      <CarouselControl direction="next" directionText="Next" onClickHandler={next} className="control-car2" />
-    </Carousel>
+    <div>
+      <div id="Aboutuscarousel" className="con1 text-center carousel slide carousel-fade" data-ride="carousel" data-interval="3000">
+            <ol className="carousel-indicators indicate">
+              <li data-target="#Aboutuscarousel" data-slide-to="0" className="active"></li>
+              <li data-target="#Aboutuscarousel" data-slide-to="1"></li>
+              <li data-target="#Aboutuscarousel" data-slide-to="2"></li>
+              <li data-target="#Aboutuscarousel" data-slide-to="3"></li>
+              <li data-target="#Aboutuscarousel" data-slide-to="4"></li>
+              <li data-target="#Aboutuscarousel" data-slide-to="5"></li>
+            </ol>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <div className="text-center">
+                  <img src={require('../../assets/about-mobile/team/1.png')} alt="1" width="60%" height="50%" className="team-imgs"/>
+                  <h5 className="head-name">Anustup Mukherjee</h5>
+                  <h6  className="pb-5 head-design"> CEO </h6>
+                </div>
+              </div>
+
+
+              <div className="carousel-item ">
+                <div className="text-center">
+                  <img src={require('../../assets/about-mobile/team/2.png')} alt="2" width="60%" height="50%" className="team-imgs"/>
+                  <h5 className="head-name">Ayush Tripathi</h5>
+                  <h6  className="pb-5 head-design"> COO &amp; CO-FOUNDER </h6>
+                </div>
+              </div>
+
+              <div className="carousel-item ">
+                <div className="text-center">
+                  <img src={require('../../assets/about-mobile/team/3.png')} alt="3" width="60%" height="50%" className="team-imgs"/>
+                  <h5 className="head-name">Ashish Sharma</h5>
+                  <h6  className="pb-5 head-design"> COO &amp; CO-FOUNDER </h6>
+                </div>
+              </div>
+
+              <div className="carousel-item ">
+                <div className="text-center">
+                  <img src={require('../../assets/about-mobile/team/4.png')} alt="4" width="60%" height="50%" className="team-imgs"/>
+                  <h5 className="head-name">Ankit Raj</h5>
+                  <h6  className="pb-5 head-design">CTO</h6>
+                </div>
+              </div>
+
+              <div className="carousel-item ">
+                <div className="text-center">
+                  <img src={require('../../assets/about-mobile/team/5.png')} alt="5" width="60%" height="50%" className="team-imgs"/>
+                  <h5 className="head-name">Ipshita Prosad</h5>
+                  <h6  className="pb-5 head-design">HOP</h6>
+                </div>
+              </div>
+
+              <div className="carousel-item ">
+                <div className="text-center">
+                  <img src={require('../../assets/about-mobile/team/6.png')} alt="6" width="60%" height="50%" className="team-imgs"/>
+                  <h5 className="head-name">Aditya Kumar</h5>
+                  <h6  className="pb-5 head-design"> DIRECTOR </h6>
+                </div>
+              </div>
+            </div>
+
+            <a className="carousel-control-prev control-car1" href="#Aboutuscarousel" role="button" data-slide="prev">
+              <span className=" car-control-sliders" aria-hidden="true"></span>
+              <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next control-car2" href="#Aboutuscarousel" role="button" data-slide="next">
+              <span className=" car-control-sliders" aria-hidden="true"></span>
+              <span className="sr-only">Next</span>
+            </a>
+
+      </div>
+    </div>
   );
 }
 
