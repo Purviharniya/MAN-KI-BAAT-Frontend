b1f0c7f77d50f7dff3a0df3b2b63b79e2cea7cca
Purvi Harniya <42908278+Purviharniya@users.noreply.github.com>
2020-07-30 14:33:40 +0530

    Update MobileHeader.js

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {Nav, Navbar} from "react-bootstrap";
 import { faUserCircle,faBars,faSearch} from "@fortawesome/free-solid-svg-icons";
+import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './MobileHeader.css'
 
@@ -8,8 +8,8 @@ class MobileHeader extends Component {
     render() {
         return (
             <div className="header-mob">
-                <Navbar style={{boxShadow:"none",marginBottom:"0"}}>
-                    <Navbar.Brand href="/">
+                <nav className="navbar navbar-expand navbar-light" style={{boxShadow:"none",marginBottom:"0"}}>
+                    <a className="navbar-brand" href="/">
                     <img
                         src={require("./images/logo.png")}
                         width="50"
@@ -17,22 +17,28 @@ class MobileHeader extends Component {
                         className="d-inline-block align-top"
                         alt="mkb-icon"
                     />
-                    </Navbar.Brand>
-                    <Nav className="ml-auto">
-                        <Nav.Link href="/"><FontAwesomeIcon icon={faUserCircle} size="2x" color="#ed257a"/></Nav.Link>
-                        <Nav.Link href="/"><FontAwesomeIcon icon={faBars} size="2x" color="#ed257a" /></Nav.Link>
-                    </Nav>
-                </Navbar>
+                    </a>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav ml-auto">                    
+                            <Link to="/">
+                                <li className="nav-item pr-3"><FontAwesomeIcon icon={faUserCircle} size="2x" color="#ed257a"/></li>
+                            </Link>
+                            <Link to="/">
+                                <li className="nav-item"><FontAwesomeIcon icon={faBars} size="2x" color="#ed257a" /></li>
+                            </Link>
+                        </ul>
+                    </div>
+                </nav>
 
                 <div className="input-group" style={{width:"80%",marginLeft:"10%"}}>
                     <input type="text" className="form-control searchbar-hw" placeholder="Search here.." />
                     <div className="input-group-append">
-                    <button className="btn btn-search-hw" type="button">
-                        <FontAwesomeIcon icon={faSearch} color="#ed257a" />
-                    </button>
+                        <button className="btn btn-search-hw" type="button">
+                            <FontAwesomeIcon icon={faSearch} color="#ed257a" />
+                        </button>
                     </div>
                 </div>
-                </div>
+            </div>
         );
     }
 }
